a1c3cad98a66ecf86a63db7d7a9506743ad8e884
Fabricio Giese <fabgiese@gmail.com>
2021-03-08 09:53:13 -0300

    add package funcoesFuncionais

---
+++ src/main/java/collections/streams/funcoesFuncionais/FluxosDeDados.java
@@ -0,0 +1,19 @@
+package collections.streams.funcoesFuncionais;
+
+public class FluxosDeDados {
+  public static void main(String[] args) {
+
+    /*
+    * Utilize uma lista com um Objeto complexo (estudante por exemplo, e realize as seguintes operações.
+    * - Transforme cada objeto em uma string com os atributos do objeto;
+    * - Conte quantos estudantes contém a lista;
+    * - Filtre estudantes com idade igual ou superior a 18 anos;
+    * - Exiba cada elemento no console;
+    * - Retorne estudantes com nome que possui a letra B;
+    * - Retorne se existir ao menos um estudante com a letra D no nome;
+    * - Retorne o estudante mais velho (maior idade) da coleção. Retorne o mais novo também.
+    * */
+
+    System.out.println("Hello World");
+  }
+}
