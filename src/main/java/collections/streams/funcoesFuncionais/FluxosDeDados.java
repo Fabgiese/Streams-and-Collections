491261eef17d81283b5e6571943f460c74ad8c25
Fabricio Giese <fabgiese@gmail.com>
2021-03-09 22:08:26 -0300

    Finished funcoesFuncionais package

--- src/main/java/collections/streams/funcoesFuncionais/FluxosDeDados.java
+++ src/main/java/collections/streams/funcoesFuncionais/FluxosDeDados.java
@@ -1,5 +1,10 @@
 package collections.streams.funcoesFuncionais;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FluxosDeDados {
   public static void main(String[] args) {
 
@@ -14,6 +19,66 @@ public class FluxosDeDados {
     * - Retorne o estudante mais velho (maior idade) da coleção. Retorne o mais novo também.
     * */
 
-    System.out.println("Hello World");
+    List<Student> estudantes = new ArrayList<>();
+
+    estudantes.add(new Student("Marina", 19, 7));
+    estudantes.add(new Student("Carlos", 20, 9));
+    estudantes.add(new Student("Mario", 13, 8));
+    estudantes.add(new Student("Francisco", 7, 1));
+    estudantes.add(new Student("Yago", 6, 1));
+    estudantes.add(new Student("Maria", 9, 3));
+    estudantes.add(new Student("Barbara", 21, 9));
+    estudantes.add(new Student("Diane", 21, 9));
+
+    System.out.println(estudantes.stream().map(student -> student.getName()
+            .concat(" - ")
+            .concat(String.valueOf(student.getAge()))
+            .concat(" - ")
+            .concat(String.valueOf(student.getGrade()))
+    ).collect(Collectors.toList()));
+
+    System.out.println("________________________");
+
+    System.out.println("Quantidade de estudantes: " + estudantes.stream().count());
+
+    System.out.println("________________________");
+
+    estudantes.stream().map(student -> student.getName()
+    .concat(" - ")
+    .concat(String.valueOf(student.getAge()))
+    .concat(" - ")
+    .concat(String.valueOf((student.getGrade()))))
+    .forEach(s -> System.out.println(s));
+
+    System.out.println("________________________");
+
+    estudantes.stream()
+            .filter(student -> student.getAge() > 17)
+            .map(student -> student.getName()
+                    .concat(" - ")
+                    .concat(String.valueOf(student.getAge()))
+                    .concat(" - ")
+                    .concat(String.valueOf((student.getGrade()))))
+            .forEach(s -> System.out.println(s));
+
+    System.out.println("________________________");
+
+    estudantes.stream()
+            .filter(student -> student.getName().contains("B"))
+            .map(student -> student.getName()
+                    .concat(" - ")
+                    .concat(String.valueOf(student.getAge()))
+                    .concat(" - ")
+                    .concat(String.valueOf((student.getGrade()))))
+            .forEach(s -> System.out.println(s));
+    System.out.println("________________________");
+
+    System.out.println(estudantes.stream().anyMatch(student -> student.getName().contains("D")));
+
+    System.out.println("________________________");
+
+    estudantes.stream().max((Comparator.comparingInt(value -> value.getAge()))).ifPresent(student -> System.out.println(student.getName() + " - " + student.getAge()));
+    estudantes.stream().min((Comparator.comparingInt(value -> value.getAge()))).ifPresent(student -> System.out.println(student.getName() + " - " + student.getAge()));
+
   }
 }
